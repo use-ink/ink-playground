b5067047c5f6025deb37816186a8a128b3529f6c
achimcc <achim@parity.io>
2021-09-03 16:18:33 +0200

    changing CLI options

--- crates/crate_extractor/src/main.rs
+++ crates/crate_extractor/src/main.rs
@@ -18,14 +18,57 @@ fn main() {
         .author("Achim S. <achim@parity.io>")
         .about("Extract Crate Data to JSON for rust analyzer")
         .arg(
-            Arg::with_name("INPUT")
-                .help("Sets the input file to use")
-                .required(false)
-                .index(1),
+            Arg::with_name("write")
+                .help("read a crate and write to a json file") // Displayed when showing help info
+                .takes_value(true) // MUST be set to true in order to be an "option" argument
+                .short("w") // This argument is triggered with "-i"
+                .long("write") // This argument is triggered with "--input"
+                .multiple(false) // Set to true if you wish to allow multiple occurrences
+                // such as "-i file -i other_file -i third_file"
+                .required(false) // By default this argument MUST be present
+                // NOTE: mutual exclusions take precedence over
+                // required arguments
+                .requires("output") // Says, "If the user uses "input", they MUST
+                // also use this other 'config' arg too"
+                // Can also specify a list using
+                // requires_all(Vec<&str>)
+                .conflicts_with("read"), // Opposite of requires(), says "if the
+                                           // user uses -a, they CANNOT use 'output'"
+                                           // also has a conflicts_with_all(Vec<&str>)
+        )
+        .arg(
+            Arg::with_name("output")
+                .help("path to json file") // Displayed when showing help info
+                .takes_value(true) // MUST be set to true in order to be an "option" argument
+                .short("o") // This argument is triggered with "-i"
+                .long("output") // This argument is triggered with "--input"
+                .multiple(false) // Set to true if you wish to allow multiple occurrences
+                // such as "-i file -i other_file -i third_file"
+                .required(false) // By default this argument MUST be present
+                // NOTE: mutual exclusions take precedence over
+                // required arguments
+                .conflicts_with("read"), // Opposite of requires(), says "if the
+                                           // user uses -a, they CANNOT use 'output'"
+                                           // also has a conflicts_with_all(Vec<&str>)
+        )
+        .arg(
+            Arg::with_name("read")
+                .help("read a json file") // Displayed when showing help info
+                .takes_value(true) // MUST be set to true in order to be an "option" argument
+                .short("r") // This argument is triggered with "-i"
+                .long("read") // This argument is triggered with "--input"
+                .multiple(false) // Set to true if you wish to allow multiple occurrences
+                // such as "-i file -i other_file -i third_file"
+                .required(false) // By default this argument MUST be present
+                // NOTE: mutual exclusions take precedence over
+                // required arguments
+                .conflicts_with("read"), // Opposite of requires(), says "if the
+                                           // user uses -a, they CANNOT use 'output'"
+                                           // also has a conflicts_with_all(Vec<&str>)
         )
         .get_matches();
 
-    let path = matches.value_of("INPUT").unwrap_or("./Cargo.toml");
+    let path = matches.value_of("write").unwrap_or("./Cargo.toml");
     let path = Path::new(path);
     let cargo_config: CargoConfig = Default::default();
     let load_cargo_config = LoadCargoConfig {
