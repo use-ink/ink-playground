ef6a6f3ca63d99151a6a55859d37afff998d3bac
achimcc <achim@parity.io>
2021-09-16 10:13:51 +0200

    add wasm-pack webpack plugin to frontend

--- crates/rust_analyzer/www/webpack.config.js
+++ crates/rust_analyzer/www/webpack.config.js
@@ -1,31 +1,34 @@
-
-const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: './src/index.tsx'
-    },
-    output: {
-        filename: 'bundle.[hash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /.tsx?$/,
-                use: 'ts-loader',
-                exclude: ['/node_modules/'],
-            },
-        ]
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            title: 'Minimal Typescript Starter',
-            template: './src/index.html'
-        })
-    ]
-}
+  mode: "development",
+  entry: {
+    app: "./src/index.tsx",
+  },
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: "Minimal Typescript Starter",
+      template: "./src/index.html",
+    }),
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, "../pkg"),
+    }),
+  ],
+};
