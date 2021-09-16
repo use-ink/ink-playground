4806753fdfbb9a7268de7114f0cd9c50edaef877
achimcc <achim@parity.io>
2021-09-16 16:18:36 +0200

    configure webpack for wasm

--- crates/playground/www/src/App.tsx
+++ crates/playground/www/src/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as wasm from "../../pkg";
 
 const App = () => {
-  const [message, setMessage] = useState<String>();
-  import("../../pkg").then((wasm) => {
+  const [message, setMessage] = useState<String>("Check");
+  useEffect(() => {
     let wasm_message = wasm.greet("ink Playground");
     setMessage(wasm_message);
   });
-  return <h1>{message}</h1>;
+
+  return (
+    <>
+      <h1>Basic TypeScript App</h1>
+      <h1>{message}</h1>
+    </>
+  );
 };
 
 export default App;
