c11e30f29395e87da2e5dc6fa88967173aab2d76
Achim Schneider <achim.schneider@posteo.de>
2021-09-25 09:47:44 +0200

    Update crates/playground/js/__tests__/Welcome.test.tsx
    Co-authored-by: Michael Müller <michi@parity.io>

--- crates/playground/js/__tests__/Welcome.test.tsx
+++ crates/playground/js/__tests__/Welcome.test.tsx
@@ -3,6 +3,6 @@ import Welcome from "../Welcome";
 
 test("Contains Welcome Message", () => {
   render(<Welcome />);
-  const linkElement = screen.getByText(/Welcome to ink Playground!/i);
+  const linkElement = screen.getByText(/Welcome to the ink! Playground!/i);
   expect(linkElement).toBeInTheDocument();
 });
