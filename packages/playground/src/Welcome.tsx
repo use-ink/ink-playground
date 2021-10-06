372904d9e1d80cc0d545a3df760ed1bebaba0465
nmauersberg <n.mauersberg@gmail.com>
2021-10-06 16:36:26 +0200

    Fix CI tests

--- packages/playground/src/Welcome.tsx
+++ packages/playground/src/Welcome.tsx
@@ -1,9 +1,5 @@
 const Welcome = () => {
-  return (
-    <>
-      <h1>Welcome to the ink! Playground!</h1>
-    </>
-  );
+  return <h1>Welcome to the ink! Playground!</h1>;
 };
 
 export default Welcome;
