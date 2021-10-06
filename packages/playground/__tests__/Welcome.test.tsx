5f39d3a52dc636af1712bfef3dd9c84feacca53a
nmauersberg <n.mauersberg@gmail.com>
2021-10-06 16:31:38 +0200

    Separate Rust and Typescript code

--- packages/playground/__tests__/Welcome.test.tsx
+++ packages/playground/__tests__/Welcome.test.tsx
@@ -1,7 +1,7 @@
-import { render, screen } from "@testing-library/react";
-import Welcome from "../Welcome";
+import { render, screen } from '@testing-library/react';
+import Welcome from '../src/Welcome';
 
-test("Contains Welcome Message", () => {
+test('Contains Welcome Message', () => {
   render(<Welcome />);
   const linkElement = screen.getByText(/Welcome to the ink! Playground!/i);
   expect(linkElement).toBeInTheDocument();
