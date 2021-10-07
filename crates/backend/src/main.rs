9400e725b9b4ac2a3e34b74c7d674c87e12b71e1
achimcc <achim@parity.io>
2021-10-07 14:04:36 +0200

    add license header to backend

--- crates/backend/src/main.rs
+++ crates/backend/src/main.rs
@@ -1,19 +1,20 @@
-use std::{path::Path};
+use std::path::Path;
 
 use actix_files as fs;
-use actix_web::{App, HttpServer};
-
-
+use actix_web::{
+    App,
+    HttpServer,
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let serve_from = "../../packages/playground/dist";
     if !Path::new(serve_from).is_dir() {
         panic!("{} is not a valid directory", serve_from);
-    } 
+    }
 
     HttpServer::new(move || {
-        App::new().service(fs::Files::new("/",  serve_from).index_file("index.html"))
+        App::new().service(fs::Files::new("/", serve_from).index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
     .run()
