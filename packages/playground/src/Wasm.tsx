80fa3a31aeb0271f3745b6a8501d8bbda5d5b0e9
nmauersberg <n.mauersberg@gmail.com>
2021-10-07 10:17:12 +0200

    Apply changes: [14](https://github.com/paritytech/ink-playground/pull/14) Was not applied because of changed paths

--- packages/playground/src/Wasm.tsx
+++ packages/playground/src/Wasm.tsx
@@ -1,28 +1,24 @@
-import { useEffect, useState } from "react";
-import * as Comlink from "comlink";
-import { Api } from "./wasm.worker";
+import { useEffect, useState } from 'react';
+import * as Comlink from 'comlink';
+import { WorkerApi } from './wasm.worker';
 
-let start = async (setter: (message: String) => void) => {
-  const handlers = await Comlink.wrap<Api>(
-    new Worker(new URL("./wasm.worker.ts", import.meta.url), {
-      type: "module",
+let start = async (setter: (message: string) => void) => {
+  const handlers = await Comlink.wrap<WorkerApi>(
+    new Worker(new URL('./wasm.worker.ts', import.meta.url), {
+      type: 'module',
     })
   ).handlers;
-  let test = await handlers.greet("ink! Playground");
+  let test = await handlers.greet('ink! Playground');
   setter(test);
 };
 
 const Wasm = () => {
-  const [message, setMessage] = useState<String>("Check");
+  const [message, setMessage] = useState<string>('Check');
   useEffect(() => {
     start(setMessage);
   });
 
-  return (
-    <>
-      <h1>{message}</h1>
-    </>
-  );
+  return <h1>{message}</h1>;
 };
 
 export default Wasm;
