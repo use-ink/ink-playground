ddd89c4918f9d39962778eacaeb27e4a06c03c8a
achimcc <achim@parity.io>
2021-10-07 08:38:05 +0200

    replace String by string

--- crates/playground/js/Wasm.tsx
+++ crates/playground/js/Wasm.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import * as Comlink from "comlink";
 import { Api } from "./wasm.worker";
 
-let start = async (setter: (message: String) => void) => {
+let start = async (setter: (message: string) => void) => {
   const handlers = await Comlink.wrap<Api>(
     new Worker(new URL("./wasm.worker.ts", import.meta.url), {
       type: "module",
@@ -13,7 +13,7 @@ let start = async (setter: (message: String) => void) => {
 };
 
 const Wasm = () => {
-  const [message, setMessage] = useState<String>("Check");
+  const [message, setMessage] = useState<string>("Check");
   useEffect(() => {
     start(setMessage);
   });
