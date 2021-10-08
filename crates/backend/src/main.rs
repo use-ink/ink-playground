a5b557f04da6855b420d0ebeadb1e395dfc11121
Michael Bock <me@thought2.de>
2021-10-08 09:02:03 +0200

    Rust fmt

--- crates/backend/src/main.rs
+++ crates/backend/src/main.rs
@@ -26,9 +26,11 @@ async fn main() -> std::io::Result<()> {
     let config = envy::from_env::<env::Config>().unwrap();
     let port = config.port;
     let frontend_folder = config.frontend_folder;
+
     if !Path::new(&frontend_folder).is_dir() {
         panic!("{} is not a valid directory.", frontend_folder);
     }
+
     HttpServer::new(move || {
         App::new()
             .wrap(
