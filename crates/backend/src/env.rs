d770fe157bdfe97feea1b01f48eaf2ec7c47c643
Michael Bock <me@thought2.de>
2021-10-08 10:48:16 +0200

    Add tests for env var parsing

--- crates/backend/src/env.rs
+++ crates/backend/src/env.rs
@@ -18,9 +18,53 @@ fn default_port() -> u16 {
     8080
 }
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Deserialize, Debug, Clone, PartialEq)]
 pub struct Config {
     #[serde(default = "default_port")]
     pub port: u16,
     pub frontend_folder: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_correct_args() {
+        let args = vec![
+            ("PORT".to_string(), "1234".to_string()),
+            ("FRONTEND_FOLDER".to_string(), "foobar".to_string()),
+        ];
+
+        let result = envy::from_iter::<_, Config>(args);
+
+        assert_eq!(
+            result,
+            Ok(Config {
+                port: 1234,
+                frontend_folder: "foobar".to_string(),
+            })
+        );
+    }
+
+    #[test]
+    fn test_missing_frontend_folder() {
+        let args = vec![("PORT".to_string(), "1234".to_string())];
+
+        let result = envy::from_iter::<_, Config>(args);
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_port_wrong_type() {
+        let args = vec![
+            ("PORT".to_string(), "\"1234\"".to_string()),
+            ("FRONTEND_FOLDER".to_string(), "foobar".to_string()),
+        ];
+
+        let result = envy::from_iter::<_, Config>(args);
+
+        assert!(result.is_err());
+    }
 }
