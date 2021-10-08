cd89dace74d438aac73996291e597b545617bd06
Michael Bock <me@thought2.de>
2021-10-08 12:57:42 +0200

    WIP

--- crates/backend/src/main.rs
+++ crates/backend/src/main.rs
@@ -21,6 +21,14 @@ use actix_web::{
 use std::path::Path;
 mod env;
 
+fn serveFrontend(dir: &str) -> actix_files::Files {
+    fs::Files::new("/", dir).index_file("index.html")
+}
+
+#[actix_web::post("/echo")]
+async fn echo(req_body: String) -> impl actix_web::Responder {
+    actix_web::HttpResponse::Ok().body(req_body)
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = envy::from_env::<env::Config>().unwrap();
@@ -32,15 +40,40 @@ async fn main() -> std::io::Result<()> {
     }
 
     HttpServer::new(move || {
+        let f = fs::Files::new("/", &frontend_folder).index_file("index.html");
         App::new()
             .wrap(
                 middleware::DefaultHeaders::new()
                     .header("Cross-Origin-Opener-Policy", "same-origin")
                     .header("Cross-Origin-Embedder-Policy", "require-corp"),
             )
-            .service(fs::Files::new("/", &frontend_folder).index_file("index.html"))
+            .service(serveFrontend(&frontend_folder))
     })
     .bind(format!("127.0.0.1:{}", port))?
     .run()
     .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{
+        http,
+        test,
+    };
+
+    // #[actix_rt::test]
+    // async fn test_index_ok() {
+    //     let req = test::TestRequest::with_header("content-type", "text/plain")
+    //         .to_http_request();
+    //     let resp = serveFrontend(req).await;
+    //     assert_eq!(resp.status(), http::StatusCode::OK);
+    // }
+
+    #[actix_rt::test]
+    async fn test_index_not_ok() {
+        let req = test::TestRequest::default().to_http_request();
+        let resp = echo("sss")(req).await;
+        assert_eq!(resp.status(), http::StatusCode::BAD_REQUEST);
+    }
 }
