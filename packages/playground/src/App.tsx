8081b977e6144d9aa2fa3d1d0401c62bce4e6709
nmauersberg <n.mauersberg@gmail.com>
2021-10-14 12:57:25 +0200

    Context - App wrapped with provider

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -1,26 +1,18 @@
-import { Layout } from './components/Layout';
-import { Header } from './components/Header';
-import { Editor } from './components/Editor';
-import { Console } from './components/Console';
-import { AppContext } from './context';
-import { Action, defaultState, reducer } from './redux/reducer';
-import { useState } from 'react';
+import { Layout } from "./components/Layout";
+import { Header } from "./components/Header";
+import { Editor } from "./components/Editor";
+import { Console } from "./components/Console";
+import { AppProvider } from "./context";
 
 const App = () => {
-  const [state, setState] = useState(defaultState)
-
-  const dispatch = (action: Action): void => {
-    setState(reducer(action, state))
-  }
-
   return (
-    <AppContext.Provider value={[state, dispatch]}>
+    <AppProvider>
       <Layout>
         <Header />
         <Editor />
         <Console />
       </Layout>
-    </AppContext.Provider>
+    </AppProvider>
   );
 };
 
