326bf4d390dfb2e6eefb21a5d5d9814b4f33a608
Michael Bock <me@thought2.de>
2021-10-14 11:05:00 +0200

    Use default state in context creation

--- packages/playground/src/context/index.ts
+++ packages/playground/src/context/index.ts
@@ -1,3 +1,4 @@
 import React from 'react';
+import { defaultState, State } from '/redux/reducer';
 
-export const EditorContext: React.Context<{}> = React.createContext({});
+export const EditorContext: React.Context<State> = React.createContext(defaultState);
