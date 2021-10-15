b84a03f2fd7aece046175c1317bf5a8aa8dfdae2
Michael Bock <me@thought2.de>
2021-10-15 07:25:29 +0200

    Run prettier

--- packages/playground/src/context/reducer.ts
+++ packages/playground/src/context/reducer.ts
@@ -2,39 +2,39 @@ export const defaultState: State = {
   darkmode: true,
   minimap: true,
   numbering: true,
-}
+};
 
 export type State = {
   darkmode: boolean;
   minimap: boolean;
   numbering: boolean;
-}
+};
 
 export type Action =
-  | { type: "SET_DARKMODE"; payload: boolean }
-  | { type: "SET_NUMBERING"; payload: boolean }
-  | { type: "SET_MINIMAP"; payload: boolean };
+  | { type: 'SET_DARKMODE'; payload: boolean }
+  | { type: 'SET_NUMBERING'; payload: boolean }
+  | { type: 'SET_MINIMAP'; payload: boolean };
 
-export type Dispatch = (action: Action) => void
+export type Dispatch = (action: Action) => void;
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case "SET_DARKMODE":
+    case 'SET_DARKMODE':
       return {
         ...state,
-        darkmode: action.payload
+        darkmode: action.payload,
       };
-    case "SET_NUMBERING":
+    case 'SET_NUMBERING':
       return {
         ...state,
-        numbering: action.payload
+        numbering: action.payload,
       };
-    case "SET_MINIMAP":
+    case 'SET_MINIMAP':
       return {
         ...state,
-        minimap: action.payload
+        minimap: action.payload,
       };
     default:
       return state;
   }
-}
+};
