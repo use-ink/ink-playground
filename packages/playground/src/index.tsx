7df412e278a1e0a1d413c22843b456e763298dc2
nmauersberg <n.mauersberg@gmail.com>
2021-10-20 11:39:55 +0200

    Add primereact components and icons

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -3,5 +3,8 @@ import App from './App';
 import './style/fonts.css';
 import './style/tailwind.css';
 import './style/global.css';
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/mdc-dark-deeppurple/theme.css';
+import 'primeicons/primeicons.css';
 
 ReactDOM.render(<App />, document.getElementById('container'));
