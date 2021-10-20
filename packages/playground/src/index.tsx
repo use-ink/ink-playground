fb4675e01b02fc17e93269bc5166aa309fce9298
nmauersberg <n.mauersberg@gmail.com>
2021-10-20 11:36:13 +0200

    Add fonts, restructure css files

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -1,5 +1,7 @@
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './styles.css';
+import './style/fonts.css';
+import './style/tailwind.css';
+import './style/global.css';
 
 ReactDOM.render(<App />, document.getElementById('container'));
