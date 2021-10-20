10d3a9e1c0cf90e288500eff4590568ba7adfd7f
Michael Bock <me@thought2.de>
2021-10-20 15:24:30 +0200

    Add side-effect functions to context

--- packages/playground/src/context/reducer.ts
+++ packages/playground/src/context/reducer.ts
@@ -2,18 +2,35 @@ export const defaultState: State = {
   darkmode: true,
   minimap: true,
   numbering: true,
+  compilation: { type: 'NOT_ASKED' },
 };
 
 export type State = {
   darkmode: boolean;
   minimap: boolean;
   numbering: boolean;
+  compilation: CompilationState;
 };
+
+export type CompilationResult =
+  | { type: 'COMPILE_OK'; payload: { result: string } }
+  | { type: 'COMPILE_ERR'; payload: { message: string } };
+
+export type CompilationState =
+  | { type: 'NOT_ASKED' }
+  | { type: 'IN_PROGRESS' }
+  | { type: 'RESULT'; payload: CompilationResult }
+  | { type: 'FAILURE'; payload: CompilationFailure };
+
+export type CompilationFailure = { type: 'COMPILATION_FAILURE'; payload: { message: string } };
 
 export type Action =
   | { type: 'SET_DARKMODE'; payload: boolean }
   | { type: 'SET_NUMBERING'; payload: boolean }
-  | { type: 'SET_MINIMAP'; payload: boolean };
+  | { type: 'SET_MINIMAP'; payload: boolean }
+  | { type: 'SET_COMPILATION_STATE'; payload: CompilationState }
+  | { type: 'START_COMPILATION'; payload: string }
+  | { type: 'RECEIVE_COMPILATION'; payload: CompilationResult };
 
 export type Dispatch = (action: Action) => void;
 
