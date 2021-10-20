b2b8f3278586165a0b6ecd9cf1cbcde2c7368948
Michael Bock <me@thought2.de>
2021-10-20 06:47:29 +0200

    Add playground test to CI

--- packages/playground/src/components/index.tsx
+++ packages/playground/src/components/index.tsx
@@ -2,6 +2,4 @@ export { Console } from './Console';
 export { Editor } from './Editor';
 export { Layout } from './Layout';
 export { Header } from './Header';
-// Temporary, will be removed:
-export { Wasm } from './WasmTest/Wasm';
 export { Welcome } from './WasmTest/Welcome';
