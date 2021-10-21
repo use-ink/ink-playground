774240d29c73fa22cb9376d767bb6d01a44decb8
nmauersberg <n.mauersberg@gmail.com>
2021-10-21 18:55:03 +0200

    Update tailwind configuration

--- packages/playground/tailwind.config.js
+++ packages/playground/tailwind.config.js
@@ -17,9 +17,22 @@ module.exports = {
     }),
     borderColor: theme => ({
       ...theme('colors'),
-      dark: '#31383D',
+      dark: '#3C3D47',
       light: '#cecece',
     }),
+    height: {
+      '5v': '5vh',
+      '10v': '10vh',
+      '20v': '20vh',
+      '30v': '30vh',
+      '40v': '40vh',
+      '50v': '50vh',
+      '60v': '60vh',
+      '70v': '70vh',
+      '80v': '80vh',
+      '90v': '90vh',
+      '100v': '100vh',
+    },
     extend: {},
   },
   variants: {
