165ef252a8c2dbfa4197ac3a26645c48fedd4efa
nmauersberg <n.mauersberg@gmail.com>
2021-10-21 10:55:08 +0200

    Fix tests, jest svg transform

--- packages/playground/__tests__/SvgImport.test.tsx
+++ packages/playground/__tests__/SvgImport.test.tsx
@@ -1,7 +1,5 @@
-import { render } from '@testing-library/react';
-import { ReactComponent as Logo } from '~/assets/ink-logo-on-dark.svg';
+import Logo from '~/assets/ink-logo-on-dark.svg';
 
 test('should import svg as React component', () => {
-  render(<Logo />);
-  expect(Logo).toBeDefined();
+  expect(Logo).toEqual('ink-logo-on-dark.svg');
 });
--- packages/playground/jest.config.js
+++ packages/playground/jest.config.js
@@ -3,14 +3,14 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+.(ts|tsx|js|jsx)$': 'ts-jest',
+    '^.+\\.svg$': '<rootDir>/jest/svgTransform.ts',
   },
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest/setupTests.ts'],
   testEnvironment: 'jsdom',
   transformIgnorePatterns: ['^react-monaco-editor', '^monaco-editor'],
   moduleNameMapper: {
     '~/(.*)': '<rootDir>/src/$1',
     '^.+.(css|scss)$': 'identity-obj-proxy',
     'monaco-editor': '<rootDir>/../../node_modules/react-monaco-editor',
-    '\\.svg': '<rootDir>/__mocks__/svgrMock.ts',
   },
 };
