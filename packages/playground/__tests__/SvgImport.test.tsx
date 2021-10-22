741519b3c3e7024ae21723b56ec3b516f217e7d0
nmauersberg <n.mauersberg@gmail.com>
2021-10-22 11:09:54 +0200

    Update svg transform: 'div' instead of filename

--- packages/playground/__tests__/SvgImport.test.tsx
+++ packages/playground/__tests__/SvgImport.test.tsx
@@ -1,5 +1,5 @@
 import Logo from '~/assets/ink-logo-on-dark.svg';
 
 test('should import svg as React component', () => {
-  expect(Logo).toEqual('ink-logo-on-dark.svg');
+  expect(Logo).toEqual('div');
 });
--- packages/playground/jest/svgTransform.ts
+++ packages/playground/jest/svgTransform.ts
@@ -1,7 +1,5 @@
-const path = require('path');
-
 module.exports = {
-  process(src, filename) {
-    return `module.exports = ${JSON.stringify(path.basename(filename))};`;
+  process() {
+    return `module.exports = 'div'`;
   },
 };
