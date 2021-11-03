54686b062f85ae4dfaa14399b17dc8d3fa119657
nmauersberg <n.mauersberg@gmail.com>
2021-11-03 14:44:18 +0100

    Remove custom tailwindcss types and style-utils

--- packages/components/src/ButtonWithIcon.tsx
+++ packages/components/src/ButtonWithIcon.tsx
@@ -1,35 +1,29 @@
 import { ReactElement, MouseEvent } from 'react';
-import { defStyles, classnames } from './style-utils';
+import { classnames } from 'tailwindcss-classnames';
 
 export type ButtonProps = {
   label: string;
-  Icon: SvgrComponent;
-  onClick: (e?: ReactMouseEvent) => void | null;
+  Icon: React.FC<React.SVGProps<SVGSVGElement>>;
+  onClick: (e?: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => void | null;
   testId?: string;
 };
 
-export type SvgrComponent = React.FC<React.SVGProps<SVGSVGElement>>;
-
-export type ReactMouseEvent = MouseEvent<HTMLButtonElement, globalThis.MouseEvent>;
-
 export const ButtonWithIcon = ({ label, Icon, onClick, testId }: ButtonProps): ReactElement => {
-  const style = defStyles({
-    button: [
-      'dark:hover:bg-elevation',
-      'hover:bg-gray-200',
-      'py-1',
-      'px-3',
-      'mr-1',
-      'rounded',
-      'text-lg',
-      'flex',
-    ],
-    icon: ['mt-1.5', 'mr-1.5'],
-  });
-
   return (
-    <button className={classnames(style.button)} onClick={(e?) => onClick(e)}>
-      <Icon className={classnames(style.icon)} data-testid={testId} />
+    <button
+      className={classnames(
+        // 'dark:hover:bg-elevation',
+        'hover:bg-gray-200',
+        'py-1',
+        'px-3',
+        'mr-1',
+        'rounded',
+        'text-lg',
+        'flex'
+      )}
+      onClick={(e?) => onClick(e)}
+    >
+      <Icon className={classnames('mt-1.5', 'mr-1.5')} data-testid={testId} />
       {label}
     </button>
   );
