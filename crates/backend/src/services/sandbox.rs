06644a6bc5a9e204dc75908774e10e1c5a735cdd
achimcc <achim@parity.io>
2021-11-03 13:21:48 +0100

    add basic secure docker command

--- crates/backend/src/services/sandbox.rs
+++ crates/backend/src/services/sandbox.rs
@@ -1,6 +1,7 @@
-use std::{fs, io, os::unix::prelude::PermissionsExt, path::PathBuf};
+use std::{fs, io, os::unix::prelude::PermissionsExt, path::PathBuf, time::Duration};
 use tempdir::TempDir;
 use snafu::{Snafu, ResultExt};
+use tokio::process::Command;
 
 pub struct Sandbox {
     #[allow(dead_code)]
@@ -19,6 +20,12 @@ pub enum Error {
 
     #[snafu(display("Unable to set permissions for output directory: {}", source))]
     UnableToSetOutputPermissions { source: io::Error },
+
+    #[snafu(display("Unable to create source file: {}", source))]
+    UnableToCreateSourceFile { source: io::Error },
+
+    #[snafu(display("Unable to set permissions for source file: {}", source))]
+    UnableToSetSourcePermissions { source: io::Error },
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
@@ -35,11 +42,14 @@ fn wide_open_permissions() -> std::fs::Permissions {
 
 #[derive(Debug, Clone)]
 pub struct CompileRequest {
+    pub code: String,
 }
 
 #[derive(Debug, Clone)]
 pub struct CompileResponse {
 }
+
+const DOCKER_PROCESS_TIMEOUT_SOFT: Duration = Duration::from_secs(10);
 
 impl Sandbox {
     pub fn new() -> Result<Self> {
@@ -57,8 +67,89 @@ impl Sandbox {
         })
     }
 
-    pub fn compile(&self, _req: &CompileRequest) -> Result<CompileResponse> {
+    pub fn compile(&self, req: &CompileRequest) -> Result<CompileResponse> {
+        self.write_source_code(&req.code)?;
+        let command = self.build_compile_command();
         Ok(CompileResponse {})
     }
+
+    fn write_source_code(&self, code: &str) -> Result<()> {
+        fs::write(&self.input_file, code).context(UnableToCreateSourceFile)?;
+        fs::set_permissions(&self.input_file, wide_open_permissions())
+            .context(UnableToSetSourcePermissions)?;
+
+        log::debug!(
+            "Wrote {} bytes of source to {}",
+            code.len(),
+            self.input_file.display()
+        );
+        Ok(())
+    }
+
+    fn build_compile_command(&self) -> Command {
+        let mut cmd = self.docker_command();
+        // set_execution_environment(&mut cmd, Some(target), &req);
+
+        // let execution_cmd = build_execution_command(Some(target), channel, mode, &req, tests);
+
+        // cmd.arg(&channel.container_name()).args(&execution_cmd);
+
+        log::debug!("Compilation command is {:?}", cmd);
+
+        cmd
+    }
+
+    fn docker_command(&self) -> Command {
+        let file_name = "lib.rs";
+
+        let mut mount_input_file = self.input_file.as_os_str().to_os_string();
+        mount_input_file.push(":");
+        mount_input_file.push("/playground/");
+        mount_input_file.push(file_name);
+
+        let mut mount_output_dir = self.output_dir.as_os_str().to_os_string();
+        mount_output_dir.push(":");
+        mount_output_dir.push("/playground-result");
+
+        let mut cmd = basic_secure_docker_command();
+
+        cmd
+            .arg("--volume").arg(&mount_input_file)
+            .arg("--volume").arg(&mount_output_dir);
+
+        cmd
+    }
+}
+
+macro_rules! docker_command {
+    ($($arg:expr),* $(,)?) => ({
+        let mut cmd = Command::new("docker");
+        $( cmd.arg($arg); )*
+        cmd
+    });
+}
+
+fn basic_secure_docker_command() -> Command {
+    let mut cmd = docker_command!(
+        "run",
+        "--detach",
+        "--cap-drop=ALL",
+        // Needed to allow overwriting the file
+        "--cap-add=DAC_OVERRIDE",
+        "--security-opt=no-new-privileges",
+        "--workdir", "/playground",
+        "--net", "none",
+        "--memory", "512m",
+        "--memory-swap", "640m",
+        "--env", format!("PLAYGROUND_TIMEOUT={}", DOCKER_PROCESS_TIMEOUT_SOFT.as_secs()),
+    );
+
+    if cfg!(feature = "fork-bomb-prevention") {
+        cmd.args(&["--pids-limit", "512"]);
+    }
+
+    cmd.kill_on_drop(true);
+
+    cmd
 }
 
