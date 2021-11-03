dbea8ae28fdb32c6577ee0a6ada308f4b8f6237a
nmauersberg <n.mauersberg@gmail.com>
2021-11-03 13:32:52 +0100

    Add and configure package dependencies

--- packages/playground/jest.config.js
+++ packages/playground/jest.config.js
@@ -12,5 +12,8 @@ module.exports = {
     '~/(.*)': '<rootDir>/src/$1',
     '^.+.(css|scss)$': 'identity-obj-proxy',
     'monaco-editor': '<rootDir>/../../node_modules/react-monaco-editor',
+    '@paritytech/components/(.*)': '<rootDir>/../components/src/$1',
+    '@paritytech/tailwindcss-classnames/(.*)':
+      '<rootDir>/../_generated/tailwindcss-classnames/src/$1',
   },
 };
