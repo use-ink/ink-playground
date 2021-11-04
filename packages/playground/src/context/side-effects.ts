a265aa1089efb0e685a4e44bc9b174f8623a5d8f
Michael Bock <me@thought2.de>
2021-11-04 16:42:38 +0100

    WIP

--- packages/playground/src/context/side-effects.ts
+++ packages/playground/src/context/side-effects.ts
@@ -1,12 +1,13 @@
 import { compileRequest } from '~/api/compile';
 import { State, Dispatch } from './reducer';
+import exampleCode from '../app/Editor/example-code';
 
 export async function compile(dispatch: Dispatch, state: State) {
   if (state.compile.type === 'IN_PROGRESS') return;
 
   dispatch({ type: 'SET_COMPILE_STATE', payload: { type: 'IN_PROGRESS' } });
 
-  const result = await compileRequest({ source: 'some rusty rust' });
+  const result = await compileRequest({ source: exampleCode });
 
   dispatch({
     type: 'SET_COMPILE_STATE',
