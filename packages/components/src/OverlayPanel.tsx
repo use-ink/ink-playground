62feb46e79375e1594591a02dcb115d02e7bb533
nmauersberg <n.mauersberg@gmail.com>
2021-11-04 13:49:53 +0100

    Add OverlayPanel and InputSwitch to components

---
+++ packages/components/src/OverlayPanel.tsx
@@ -0,0 +1,3 @@
+import './style/overlay-panel.css';
+
+export { OverlayPanel } from 'primereact/overlaypanel';
--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -1,1 +1,7 @@
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 export { ButtonWithIcon } from './ButtonWithIcon';
+export { OverlayPanel } from './OverlayPanel';
+export { InputSwitch } from './InputSwitch';
