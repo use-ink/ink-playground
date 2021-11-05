5049f4d7b2e85d4c3340c4b0826837af5dd95b61
Michael Bock <me@thought2.de>
2021-11-05 18:15:41 +0100

    Run rust format

--- crates/sandbox/src/docker_command.rs
+++ crates/sandbox/src/docker_command.rs
@@ -1,3 +1,23 @@
+// Copyright 2018-2021 Parity Technologies (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! This module contains the compile service of the backend. It receives a
+//! string of Rust source code and returns the result of compiling the code.
+//! For security reason we run the compilation inside a Docker container.
+//! In order to ease testing, the service is parameterized by a compile
+//! strategy. This allows easy mocking.
+
 #[macro_export]
 macro_rules! docker_command {
     ($($arg:expr),* $(,)?) => ({
