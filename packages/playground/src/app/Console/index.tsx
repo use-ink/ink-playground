b880fcc0da8bd810b2a2b841920d18036cfee954
nmauersberg <n.mauersberg@gmail.com>
2021-11-05 16:00:38 +0100

    Set console height and dark mode

--- packages/playground/src/app/Console/index.tsx
+++ packages/playground/src/app/Console/index.tsx
@@ -1,3 +1,7 @@
 export const Console = () => {
-  return <h2 className="m-4">Console</h2>;
+  return (
+    <div className="consoleHeight dark:text-primary dark:bg-primary text-light dark:border-dark border-light border-t">
+      <h2 className="p-4">Console</h2>
+    </div>
+  );
 };
