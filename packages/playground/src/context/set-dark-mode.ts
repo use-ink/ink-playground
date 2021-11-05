f5fe43e056f45034617d0ebb65fb6f3e5e933710
nmauersberg <n.mauersberg@gmail.com>
2021-11-05 16:00:21 +0100

    Add dark mode utility to context

---
+++ packages/playground/src/context/set-dark-mode.ts
@@ -0,0 +1,8 @@
+export const setDarkMode = (payload: boolean): void => {
+  const root = window.document.documentElement;
+  if (payload) {
+    root.classList.add('dark');
+  } else {
+    root.classList.remove('dark');
+  }
+};
