28d46f3654c2e3c4624075dce18563cc60c2258c
nmauersberg <n.mauersberg@gmail.com>
2021-11-10 10:25:55 +0100

    Layout with splitter and styling

--- packages/playground/src/app/index.tsx
+++ packages/playground/src/app/index.tsx
@@ -4,14 +4,21 @@ import { Layout } from './Layout';
 import { Header } from './Header';
 import { AppProvider } from '~/context';
 import { ReactElement } from 'react';
+import { Splitter, SplitterPanel } from '@paritytech/components/';
 
 const App = (): ReactElement => {
   return (
     <AppProvider>
       <Layout>
         <Header />
-        <Editor />
-        <Console />
+        <Splitter className="content" layout="vertical" gutterSize={6}>
+          <SplitterPanel size={80} minSize={0}>
+            <Editor />
+          </SplitterPanel>
+          <SplitterPanel size={20} minSize={0}>
+            <Console />
+          </SplitterPanel>
+        </Splitter>
       </Layout>
     </AppProvider>
   );
