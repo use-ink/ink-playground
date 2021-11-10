0e8951b89dfd4fa5bb2a3bce86068297500c3fda
nmauersberg <n.mauersberg@gmail.com>
2021-11-10 16:15:17 +0100

    Scrollbar styling

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -3,5 +3,6 @@ import App from './app';
 import './style/fonts.css';
 import './style/tailwind.css';
 import './style/global.css';
+import './style/scrollbar.css';
 
 ReactDOM.render(<App />, document.getElementById('container'));
