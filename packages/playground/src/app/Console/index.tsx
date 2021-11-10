cf522eb5a39cea44826d491559c3513eff383346
nmauersberg <n.mauersberg@gmail.com>
2021-11-10 16:15:17 +0100

    Scrollbar styling

--- packages/playground/src/app/Console/index.tsx
+++ packages/playground/src/app/Console/index.tsx
@@ -2,7 +2,18 @@ export const Console = () => {
   return (
     <div className="h-full w-full relative">
       <div className="dark:text-primary dark:bg-primary text-light text-sm subpixel-antialiased leading-normal px-5 pt-4 pb-6 shadow-lg overflow-y-scroll h-full w-full absolute">
-        <p>Console</p>
+        <p>Testentry</p>
+        <p>Testentry</p>
+        <p>Testentry</p>
+        <p>Testentry</p>
+        <p>Testentry</p>
+        <p>Testentry</p>
+        <p>Testentry</p>
+        <p>Testentry</p>
+        <p>Testentry</p>
+        <p>Testentry</p>
+        <p>Testentry</p>
+        <p>Testentry</p>
       </div>
     </div>
   );
