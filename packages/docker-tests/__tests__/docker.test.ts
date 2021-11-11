6a20717963c0946d71ef0387ca4719a5a1a37c5d
Michael Bock <me@thought2.de>
2021-11-11 07:13:14 +0100

    Add test for compile endpoint

--- packages/docker-tests/__tests__/docker.test.ts
+++ packages/docker-tests/__tests__/docker.test.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost';
 
 describe('Given the server is up and running', () => {
-  test('When a request to the correct URL is made', async () => {
+  test('When a request to the status route is made', async () => {
     await expect(axios.get(`${BACKEND_URL}/status`)).resolves.toMatchObject({
       status: 200,
       data: 'ink-backend is live',
@@ -14,5 +16,19 @@ describe('Given the server is up and running', () => {
 describe('Given the server provides the built frontend', () => {
   test('When a request is made', async () => {
     await expect(axios.get(`${BACKEND_URL}/`)).resolves.toHaveProperty('status', 200);
+  });
+});
+
+describe('Given the server provides a working compile endpoint', () => {
+  test('When a compile request is made', async () => {
+    const source = fs
+      .readFileSync(path.join(__dirname, '../../../crates/contract/lib.rs'))
+      .toString();
+
+    await expect(
+      axios.post(`${BACKEND_URL}/compile`, {
+        source,
+      })
+    ).resolves.toMatchObject({ status: 200, data: { type: 'SUCCESS' } });
   });
 });
