102b34b67916f9d8581fb9af9eecee3c47a4dcfb
nmauersberg <n.mauersberg@gmail.com>
2021-11-12 12:54:22 +0100

    Add "ConsoleMessages" appearance tests

--- packages/components/src/console/index.tsx
+++ packages/components/src/console/index.tsx
@@ -3,11 +3,17 @@ import { ConsoleMessage } from './ConsoleMessage';
 import { Message } from './ConsoleMessage';
 
 export const ConsoleMessages = ({ messages }: { messages: Message[] }): ReactElement => {
+  const AlwaysScrollToBottom = () => {
+    const elementRef = useRef<null | HTMLDivElement>(null);
+    useEffect(() => elementRef?.current?.scrollIntoView());
+    return <div ref={elementRef} />;
+  };
+
   const Messages = () => {
     return (
       <>
         {messages.map((message, index) => {
-          return <ConsoleMessage key={index} message={message} />;
+          return <ConsoleMessage key={index} mIndex={index} message={message} />;
         })}
       </>
     );
@@ -19,10 +25,4 @@ export const ConsoleMessages = ({ messages }: { messages: Message[] }): ReactEle
       <AlwaysScrollToBottom />
     </>
   );
-};
-
-const AlwaysScrollToBottom = () => {
-  const elementRef = useRef<null | HTMLDivElement>(null);
-  useEffect(() => elementRef?.current?.scrollIntoView());
-  return <div ref={elementRef} />;
 };
