1b965e18105e2fe21fbca6cfc6259da48db11fbc
achimcc <achim@parity.io>
2021-11-16 15:50:02 +0100

    refactor wasm worker part

--- packages/playground/src/app/Editor/utils/wasm.worker.ts
+++ packages/playground/src/app/Editor/utils/wasm.worker.ts
@@ -2,7 +2,7 @@ import * as Comlink from 'comlink';
 
 async function initHandlers(): Promise<any> {
   // If threads are unsupported in this browser, skip this handler.
-  const rust_wasm = await import('../../../pkg');
+  const rust_wasm = await import('../../../../pkg/rust_analyzer_wasm');
   await rust_wasm.default();
   const numThreads = navigator.hardwareConcurrency;
   // must be included to init rayon thread pool with web workers
