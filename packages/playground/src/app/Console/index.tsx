358de3b3aa7694641c94f3c021b84a8c0fc681c6
nmauersberg <n.mauersberg@gmail.com>
2021-11-17 16:21:17 +0100

    Refactor console message filtering

--- packages/playground/src/app/Console/index.tsx
+++ packages/playground/src/app/Console/index.tsx
@@ -1,37 +1,38 @@
-import { ReactElement, useContext, useEffect } from 'react';
+import { ReactElement, useContext, useMemo } from 'react';
 import { ConsoleMessages } from '@paritytech/components/';
 import { MessageContext } from '~/context/messages';
 import { Message } from '@paritytech/components/';
 
 export const Console = (): ReactElement => {
   const [state] = useContext(MessageContext);
-  let processedMessages: Message[] = [];
 
-  // Find and return the last message object in message state with a given ID
-  const findLast = (id: number): Message | undefined => {
-    const messagesOfConcern = state.messages.filter(message => message.id === id);
-    if (messagesOfConcern.length > 1) return messagesOfConcern[messagesOfConcern.length - 1];
-  };
+  const filteredMessages = useMemo(() => filterMessages(state.messages), [state.messages]);
 
-  // Evaluate if message object with given id and index is the first occurrence in message state
-  const isFirst = (id: number, index: number): boolean => {
-    const indexFound = state.messages.map(m => m.id).indexOf(id);
-    return indexFound === index ? true : false;
-  };
+  return <ConsoleMessages messages={filteredMessages} />;
+};
 
-  useEffect(() => {
-    const messagesToRender: Message[] = [];
-    state.messages.forEach((message, index) => {
-      // Get the most recent message state from context
-      const latestMessage = findLast(message.id);
-      // If there is content to update, replace message
-      if (latestMessage) message = latestMessage;
-      // Only push the first occurrence to the messagesToRender
-      // To preserve original message order
-      if (isFirst(message.id, index)) messagesToRender.push(message);
-    });
-    processedMessages = messagesToRender;
-  }, [state.messages]);
+const filterMessages = (messages: Message[]): Message[] => {
+  const messagesToRender: Message[] = [];
+  messages.forEach((message, index) => {
+    // Get the most recent message state from context
+    const latestMessage = findLast(messages, message.id);
+    // If there is content to update, replace message
+    if (latestMessage) message = latestMessage;
+    // Only push the first occurrence to the messagesToRender
+    // To preserve original message order
+    if (isFirst(messages, message.id, index)) messagesToRender.push(message);
+  });
+  return messagesToRender;
+};
 
-  return <ConsoleMessages messages={processedMessages} />;
+// Find and return the last message object in message state with a given ID
+const findLast = (messages: Message[], id: number): Message | undefined => {
+  const messagesOfConcern = messages.filter(message => message.id === id);
+  if (messagesOfConcern.length > 1) return messagesOfConcern[messagesOfConcern.length - 1];
+};
+
+// Evaluate if message object with given id and index is the first occurrence in message state
+const isFirst = (messages: Message[], id: number, index: number): boolean => {
+  const indexFound = messages.map(m => m.id).indexOf(id);
+  return indexFound === index ? true : false;
 };
