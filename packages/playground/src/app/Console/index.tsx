2ad27655e6d266d6ca1a388198fd048f81d365cd
nmauersberg <n.mauersberg@gmail.com>
2021-11-17 13:02:26 +0100

    Move console styling to components package

--- packages/playground/src/app/Console/index.tsx
+++ packages/playground/src/app/Console/index.tsx
@@ -27,16 +27,5 @@ export const Console = (): ReactElement => {
     setProcessedMessages(messagesToRender);
   }, [state.messages]);
 
-  return (
-    <div className="h-full w-full relative">
-      <div
-        className={`
-          dark:text-primary dark:bg-primary text-light text-sm subpixel-antialiased  
-          leading-normal px-5 pt-4 pb-4 shadow-lg overflow-y-scroll h-full w-full absolute
-        `}
-      >
-        <ConsoleMessages messages={processedMessages} />
-      </div>
-    </div>
-  );
+  return <ConsoleMessages messages={processedMessages} />;
 };
