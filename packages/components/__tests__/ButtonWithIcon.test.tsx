fbc9e8a64490239e539df6e6eaaad94ab75464f2
nmauersberg <n.mauersberg@gmail.com>
2021-11-17 10:22:50 +0100

    ButtonWithIcon disabled and loading props added

--- packages/components/__tests__/ButtonWithIcon.test.tsx
+++ packages/components/__tests__/ButtonWithIcon.test.tsx
@@ -30,11 +30,153 @@ describe('Given the ButtonWithIcon component is rendered', () => {
   });
 
   test('When Button is clicked', async () => {
-    // Given, When
+    // Given
     const testButton = await screen.findByText('testButton');
+    // When
+    testButton.click();
+    // Then ...
+    expect(wasClicked).toBeCalledTimes(1);
+  });
+});
+
+describe('Given Button states were changed', () => {
+  test('Given Button is rendered in disabled state', async () => {
+    // Given, When
+    const wasClicked: () => void = jest.fn();
+    render(
+      <ButtonWithIcon
+        label="testButtonDisabled"
+        Icon={DemoSvg}
+        testId={'buttonIcon'}
+        disabled={true}
+        onClick={() => {
+          wasClicked();
+        }}
+      />
+    );
+    const testButton = await screen.findByText('testButtonDisabled');
+    const disabledIcon = await screen.findByTestId('icon-disabled');
+
+    // Then ...
+    expect(testButton).toBeInTheDocument();
+    expect(testButton).toHaveClass('cursor-not-allowed');
+    expect(disabledIcon).toBeInTheDocument();
+  });
+
+  test('Given disabled Button is clicked', async () => {
+    // Given
+    const wasClicked: () => void = jest.fn();
+    render(
+      <ButtonWithIcon
+        label="testButtonDisabled"
+        Icon={DemoSvg}
+        testId={'buttonIcon'}
+        disabled={true}
+        onClick={() => {
+          wasClicked();
+        }}
+      />
+    );
+    const testButton = await screen.findByText('testButtonDisabled');
+
+    // When
     testButton.click();
 
     // Then ...
-    expect(wasClicked).toBeCalledTimes(1);
+    expect(wasClicked).toBeCalledTimes(0);
+  });
+
+  test('Given Button is rendered in loading state', async () => {
+    // Given, When
+    const wasClicked: () => void = jest.fn();
+    render(
+      <ButtonWithIcon
+        label="testButtonLoading"
+        Icon={DemoSvg}
+        testId={'buttonIcon'}
+        loading={true}
+        onClick={() => {
+          wasClicked();
+        }}
+      />
+    );
+    const testButton = await screen.findByText('testButtonLoading');
+    const loadingIcon = await screen.findByTestId('icon-loading');
+
+    // Then ...
+    expect(testButton).toBeInTheDocument();
+    expect(testButton).toHaveClass('cursor-not-allowed');
+    expect(loadingIcon).toBeInTheDocument();
+  });
+
+  test('Given loading Button is clicked', async () => {
+    // Given
+    const wasClicked: () => void = jest.fn();
+    render(
+      <ButtonWithIcon
+        label="testButtonLoading"
+        Icon={DemoSvg}
+        testId={'buttonIcon'}
+        loading={true}
+        onClick={() => {
+          wasClicked();
+        }}
+      />
+    );
+    const testButton = await screen.findByText('testButtonLoading');
+
+    // When
+    testButton.click();
+
+    // Then ...
+    expect(wasClicked).toBeCalledTimes(0);
+  });
+
+  test('Given Button is rendered in loading AND disabled state', async () => {
+    // Given, When
+    const wasClicked: () => void = jest.fn();
+    render(
+      <ButtonWithIcon
+        label="testButtonLoadingDisabled"
+        Icon={DemoSvg}
+        testId={'buttonIcon'}
+        loading={true}
+        disabled={true}
+        onClick={() => {
+          wasClicked();
+        }}
+      />
+    );
+    const testButton = await screen.findByText('testButtonLoadingDisabled');
+    const loadingIcon = await screen.findByTestId('icon-loading');
+
+    // Then ...
+    expect(testButton).toBeInTheDocument();
+    expect(testButton).toHaveClass('cursor-not-allowed');
+    expect(loadingIcon).toBeInTheDocument();
+  });
+
+  test('Given loading AND disabled Button is clicked', async () => {
+    // Given
+    const wasClicked: () => void = jest.fn();
+    render(
+      <ButtonWithIcon
+        label="testButtonLoadingDisabled"
+        Icon={DemoSvg}
+        testId={'buttonIcon'}
+        loading={true}
+        disabled={true}
+        onClick={() => {
+          wasClicked();
+        }}
+      />
+    );
+    const testButton = await screen.findByText('testButtonLoadingDisabled');
+
+    // When
+    testButton.click();
+
+    // Then ...
+    expect(wasClicked).toBeCalledTimes(0);
   });
 });
--- packages/components/src/ButtonWithIcon.tsx
+++ packages/components/src/ButtonWithIcon.tsx
@@ -7,15 +7,47 @@ export type ButtonProps = {
     e?: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
   ) => void | Promise<void> | null;
   testId?: string;
+  disabled?: boolean;
+  loading?: boolean;
 };
 
-export const ButtonWithIcon = ({ label, Icon, onClick, testId }: ButtonProps): ReactElement => {
+export const ButtonWithIcon = ({
+  label,
+  Icon,
+  onClick,
+  testId,
+  disabled,
+  loading,
+}: ButtonProps): ReactElement => {
+  const isLoading = loading !== undefined ? loading : false;
+  const isDisabled = isLoading ? true : disabled !== undefined ? disabled : false;
+
+  const disabledClasses = isDisabled ? 'cursor-not-allowed text-gray-600 bg-elevation' : '';
+
+  const IconOfState = (): ReactElement => {
+    if (isLoading) {
+      return (
+        <i className="pi pi-spinner animate-spin mt-1.5 mr-1.5" data-testid={'icon-loading'} />
+      );
+    }
+    return (
+      <>
+        {isDisabled ? (
+          <i className="pi pi-ban mt-1.5 mr-1.5" data-testid={'icon-disabled'} />
+        ) : (
+          <Icon className={'mt-1.5 mr-1.5'} data-testid={testId} />
+        )}
+      </>
+    );
+  };
+
   return (
     <button
-      className={'dark:hover:bg-elevation hover:bg-gray-200 py-1 px-3 mr-1 rounded text-lg flex'}
+      disabled={isDisabled}
+      className={`${disabledClasses} dark:hover:bg-elevation hover:bg-gray-200 py-1 px-3 mr-1 rounded text-lg flex`}
       onClick={(e?) => onClick(e)}
     >
-      <Icon className={'mt-1.5 mr-1.5'} data-testid={testId} />
+      <IconOfState />
       {label}
     </button>
   );
