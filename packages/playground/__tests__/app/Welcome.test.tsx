107f5a9ddb04555aa0da43d10e833b5b44b1e214
achimcc <achim@parity.io>
2021-11-18 13:00:34 +0100

    delete welcome test

--- packages/playground/__tests__/app/Welcome.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Welcome } from '~/app/WasmTest/Welcome';
-
-test('should render the "Welcome" component', () => {
-  render(<Welcome />);
-  const linkElement = screen.getByText('Welcome to the ink! Playground!');
-  expect(linkElement).toBeInTheDocument();
-});
