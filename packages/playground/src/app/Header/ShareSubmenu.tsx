688c0f16956c1d357a61294115cc3dd48ed82794
nmauersberg <n.mauersberg@gmail.com>
2021-11-19 17:12:47 +0100

    Implement share OverlayPanel

---
+++ packages/playground/src/app/Header/ShareSubmenu.tsx
@@ -0,0 +1,19 @@
+import { ButtonWithIcon } from '@paritytech/components/';
+import { ReactElement } from 'react';
+import { GistIcon } from '~/symbols';
+
+export const ShareSubmenu = (): ReactElement => {
+  return (
+    <div className="mr-8 mt-4 w-44">
+      <ButtonWithIcon
+        // non-breaking space "\u00A0"
+        label={'Create\u00A0Gist'}
+        Icon={GistIcon}
+        testId={'buttonIcon'}
+        onClick={() => {
+          alert('Share Gist!');
+        }}
+      />
+    </div>
+  );
+};
