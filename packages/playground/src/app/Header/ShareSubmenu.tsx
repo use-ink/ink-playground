de21bb51d340e56ceb8af69ee00b188566b4d2fd
nmauersberg <n.mauersberg@gmail.com>
2021-11-24 11:46:59 +0100

    Style menu options as row component with border

--- packages/playground/src/app/Header/ShareSubmenu.tsx
+++ packages/playground/src/app/Header/ShareSubmenu.tsx
@@ -1,11 +1,12 @@
-import { ButtonWithIcon } from '@paritytech/components/';
+import { MenuOptionWithIcon } from '@paritytech/components/';
 import { ReactElement } from 'react';
 import { GistIcon } from '~/symbols';
 
 export const ShareSubmenu = (): ReactElement => {
   return (
-    <div className="mr-8 mt-4 w-44">
-      <ButtonWithIcon
+    <div className="w-44">
+      <h2 className="px-2 pt-1 pb-2">Share Options</h2>
+      <MenuOptionWithIcon
         // non-breaking space "\u00A0"
         label={'Create\u00A0Gist'}
         Icon={GistIcon}
