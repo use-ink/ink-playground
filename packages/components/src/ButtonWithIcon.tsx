fd69be1c00f94a60b4015c0f0636152f1f4023f9
nmauersberg <n.mauersberg@gmail.com>
2021-11-24 14:49:25 +0100

    ShareMenu styling, Button add "bg-primary"

--- packages/components/src/ButtonWithIcon.tsx
+++ packages/components/src/ButtonWithIcon.tsx
@@ -12,7 +12,9 @@ export type ButtonProps = {
 export const ButtonWithIcon = ({ label, Icon, onClick, testId }: ButtonProps): ReactElement => {
   return (
     <button
-      className={'dark:hover:bg-elevation hover:bg-gray-200 py-1 px-3 mr-1 rounded text-lg flex'}
+      className={
+        'dark:hover:bg-elevation hover:bg-gray-200 bg-primary py-1 px-3 mr-1 rounded text-lg flex'
+      }
       onClick={(e?) => onClick(e)}
     >
       <Icon className={'mt-1.5 mr-1.5'} data-testid={testId} />
--- packages/playground/src/app/Header/ShareSubmenu.tsx
+++ packages/playground/src/app/Header/ShareSubmenu.tsx
@@ -4,7 +4,7 @@ import { GistIcon } from '~/symbols';
 
 export const ShareSubmenu = (): ReactElement => {
   return (
-    <div className="mr-8 mt-4 w-44">
+    <div className="mr-8 mt-4 flex">
       <ButtonWithIcon
         // non-breaking space "\u00A0"
         label={'Create\u00A0Gist'}
