a3617de907032e5cca28778018e85b4cf2c4d620
nmauersberg <n.mauersberg@gmail.com>
2021-11-24 17:49:27 +0100

    Add LabeledLink and MenuElementWrapper components

---
+++ packages/components/src/LabeledLink.tsx
@@ -0,0 +1,27 @@
+import { ReactElement } from 'react';
+
+export type LabeledLinkProps = {
+  label: string;
+  link: string;
+};
+
+export const LabeledLink = ({ label, link }: LabeledLinkProps): ReactElement => {
+  return (
+    <div className="dark:bg-elevation dark:border-dark border-light border-t last:rounded-b py-2 px-4 w-full text-lg">
+      <p className="text-sm mt-1 mb-2">{label}</p>
+      <div className="dark:bg-primary dark:border-dark bg-gray-200 border-light last:rounded-b py-2 px-4 w-full text-lg flex justify-between items-center rounded">
+        <a href={link} target="_blank" className="text-sm">
+          {link}
+        </a>
+        <button
+          onClick={() => {
+            // Maybe we could use an extra lib for that
+            navigator.clipboard.writeText(link);
+          }}
+        >
+          <i className="pi pi-copy ml-4" />
+        </button>
+      </div>
+    </div>
+  );
+};
