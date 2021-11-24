083f60fb8ccd56c07f45908c598c6cc761aa672f
nmauersberg <n.mauersberg@gmail.com>
2021-11-24 17:50:45 +0100

    ButtonWithIcon optional dropdown design

--- packages/components/src/ButtonWithIcon.tsx
+++ packages/components/src/ButtonWithIcon.tsx
@@ -9,6 +9,7 @@ export type ButtonProps = {
   testId?: string;
   disabled?: boolean;
   loading?: boolean;
+  isMenuOption?: boolean;
 };
 
 export const ButtonWithIcon = ({
@@ -18,6 +19,7 @@ export const ButtonWithIcon = ({
   testId,
   disabled,
   loading,
+  isMenuOption = false,
 }: ButtonProps): ReactElement => {
   const disabledClasses =
     disabled || loading
@@ -25,21 +27,25 @@ export const ButtonWithIcon = ({
       : '';
 
   const IconOfState = (): ReactElement => {
-    if (loading)
-      return (
-        <i className="pi pi-spinner animate-spin mt-1.5 mr-1.5 w-4" data-testid={'icon-loading'} />
-      );
+    const iconStyle = 'mt-1.5 mr-1.5 w-4';
+    const spinnerIcon = `pi pi-spinner animate-spin ${iconStyle}`;
+    const disabledIcon = `pi pi-ban ${iconStyle}`;
 
-    if (disabled)
-      return <i className="pi pi-ban mt-1.5 mr-1.5 w-4" data-testid={'icon-disabled'} />;
-
-    return <Icon className={'mt-1.5 mr-1.5 w-4'} data-testid={testId} />;
+    if (loading) return <i className={spinnerIcon} data-testid={'icon-loading'} />;
+    if (disabled) return <i className={disabledIcon} data-testid={'icon-disabled'} />;
+    return <Icon className={iconStyle} data-testid={testId} />;
   };
+
+  const menuOptionStyle =
+    'dark:hover:bg-elevation-3 bg-gray-100 hover:bg-gray-200 dark:bg-elevation-1 dark:border-dark border-light border-t last:rounded-b py-2 px-4 w-full text-lg flex';
+
+  const buttonStyle =
+    'dark:hover:bg-elevation hover:bg-gray-200 py-1 px-3 mr-1 rounded text-lg flex';
 
   return (
     <button
       disabled={disabled || loading}
-      className={`${disabledClasses} dark:hover:bg-elevation hover:bg-gray-200 py-1 px-3 mr-1 rounded text-lg flex`}
+      className={`${disabledClasses} ${isMenuOption ? menuOptionStyle : buttonStyle}`}
       onClick={(e?) => onClick(e)}
     >
       <IconOfState />
