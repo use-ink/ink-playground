bf137fcbd5bc22a179550d539373ffad7f18fb4d
Michael Bock <me@thought2.de>
2021-11-25 10:55:38 +0100

    Add gists API endpoints to frontend

---
+++ packages/playground/src/api/gists/load.ts
@@ -0,0 +1,50 @@
+import { COMPILE_URL, GIST_LOAD_URL } from '~/env';
+import * as Common from '@paritytech/commontypes';
+
+// -------------------------------------------------------------------------------------------------
+// Types
+// -------------------------------------------------------------------------------------------------
+
+export type GistLoadApiRequest = Common.GistLoadRequest;
+
+export type GistLoadApiResponse =
+  | {
+      type: 'OK';
+      payload: Common.GistLoadResponse;
+    }
+  | {
+      type: 'NETWORK_ERROR';
+    }
+  | {
+      type: 'SERVER_ERROR';
+      payload: { status: number };
+    };
+
+// -------------------------------------------------------------------------------------------------
+// Functions
+// -------------------------------------------------------------------------------------------------
+
+const mapResponse = async (response: Response): Promise<GistLoadApiResponse> =>
+  response.status === 200
+    ? {
+        type: 'OK',
+        payload: await response.json(),
+      }
+    : {
+        type: 'SERVER_ERROR',
+        payload: { status: response.status },
+      };
+
+export const gistLoadRequest = (request: GistLoadApiRequest): Promise<GistLoadApiResponse> => {
+  const opts: RequestInit = {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'same-origin',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(request),
+  };
+
+  return fetch(GIST_LOAD_URL || '', opts)
+    .then(mapResponse)
+    .catch(() => ({ type: 'NETWORK_ERROR' }));
+};
