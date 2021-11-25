e74f308413b1401cf6052ed75def79feac6d25b4
Michael Bock <me@thought2.de>
2021-11-25 10:55:38 +0100

    Add gists API endpoints to frontend

---
+++ packages/playground/src/api/gists/create.ts
@@ -0,0 +1,52 @@
+import { COMPILE_URL, GIST_CREATE_URL } from '~/env';
+import * as Common from '@paritytech/commontypes';
+
+// -------------------------------------------------------------------------------------------------
+// Types
+// -------------------------------------------------------------------------------------------------
+
+export type GistCreateApiRequest = Common.GistCreateRequest;
+
+export type GistCreateApiResponse =
+  | {
+      type: 'OK';
+      payload: Common.GistCreateResponse;
+    }
+  | {
+      type: 'NETWORK_ERROR';
+    }
+  | {
+      type: 'SERVER_ERROR';
+      payload: { status: number };
+    };
+
+// -------------------------------------------------------------------------------------------------
+// Functions
+// -------------------------------------------------------------------------------------------------
+
+const mapResponse = async (response: Response): Promise<GistCreateApiResponse> =>
+  response.status === 200
+    ? {
+        type: 'OK',
+        payload: await response.json(),
+      }
+    : {
+        type: 'SERVER_ERROR',
+        payload: { status: response.status },
+      };
+
+export const gistCreateRequest = (
+  request: GistCreateApiRequest
+): Promise<GistCreateApiResponse> => {
+  const opts: RequestInit = {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'same-origin',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(request),
+  };
+
+  return fetch(GIST_CREATE_URL || '', opts)
+    .then(mapResponse)
+    .catch(() => ({ type: 'NETWORK_ERROR' }));
+};
--- packages/playground/src/env.ts
+++ packages/playground/src/env.ts
@@ -1,1 +1,5 @@
 export const COMPILE_URL: string | undefined = process.env.COMPILE_URL;
+
+export const GIST_CREATE_URL: string | undefined = process.env.GIST_CREATE_URL;
+
+export const GIST_LOAD_URL: string | undefined = process.env.GIST_LOAD_URL;
