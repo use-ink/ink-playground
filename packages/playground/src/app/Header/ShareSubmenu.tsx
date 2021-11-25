b266126769c31be0b15a1f300213a2474d4623bf
Michael Bock <me@thought2.de>
2021-11-25 16:08:27 +0100

    Render API result

--- packages/playground/src/app/Header/ShareSubmenu.tsx
+++ packages/playground/src/app/Header/ShareSubmenu.tsx
@@ -1,20 +1,62 @@
 import { ButtonWithIcon, LabeledLink } from '@paritytech/components/';
-import { ReactElement, useContext, useState } from 'react';
+import { ReactElement, ReactNode, useContext, useState } from 'react';
 import { GithubIcon } from '~/symbols';
-import { Dispatch, State } from '~/context/app/reducer';
+import { Dispatch, GistState, State } from '~/context/app/reducer';
 import { gistCreate } from '~/context/app/side-effects/gists/create';
 import { AppContext } from '~/context/app/';
 import { MessageContext } from '~/context/messages/';
 import { MessageState, MessageDispatch } from '~/context/messages/reducer';
+import { Gist, GistCreateResponse } from '@paritytech/commontypes';
+import { GistCreateApiResponse } from '~/api/gists';
 
 const playgroundLink = 'https://ink-playground.netlify.app/?id=375eb5406914a37d5009842811f4f426';
 const gistLink = 'https://gist.github.com/375eb5406914a37d5009842811f4f426';
 
-export const ShareSubmenu = (): ReactElement => {
-  const [isLoading] = useState(false);
+const ViewError = ({ message }: { message: string }) => <div>Opps!</div>;
 
+const ViewGist = ({ gist }: { gist: Gist }) => (
+  <>
+    <LabeledLink label="Link to Playground:" link={playgroundLink} />
+    <LabeledLink label="Link to GitHub Gist:" link={gistLink} />
+  </>
+);
+
+const GistCreateResponse = ({ response }: { response: GistCreateResponse }): ReactElement => {
+  switch (response.type) {
+    case 'SUCCESS':
+      return <ViewGist gist={response.payload} />;
+    case 'ERROR':
+      return <ViewError message={`Gist creation error: ${response.payload}`} />;
+  }
+};
+
+const ApiResponse = ({ response }: { response: GistCreateApiResponse }): ReactElement => {
+  switch (response.type) {
+    case 'OK':
+      return <GistCreateResponse response={response.payload} />;
+    case 'NETWORK_ERROR':
+      return <ViewError message="Network Error" />;
+    case 'SERVER_ERROR':
+      return <ViewError message="Server Error" />;
+  }
+};
+
+const GistState = ({ gistState }: { gistState: GistState }): ReactElement | null => {
+  switch (gistState.type) {
+    case 'NOT_ASKED':
+      return null;
+    case 'IN_PROGRESS':
+      return null;
+    case 'RESULT':
+      return <ApiResponse response={gistState.payload} />;
+  }
+};
+
+export const ShareSubmenu = (): ReactElement => {
   const [state, dispatch]: [State, Dispatch] = useContext(AppContext);
   const [, dispatchMessage]: [MessageState, MessageDispatch] = useContext(MessageContext);
+
+  const isLoading = state.gist.type === 'IN_PROGRESS';
 
   return (
     <>
@@ -29,12 +71,7 @@ export const ShareSubmenu = (): ReactElement => {
         isMenuOption={true}
         loading={isLoading}
       />
-      {true && (
-        <>
-          <LabeledLink label="Link to Playground:" link={playgroundLink} />
-          <LabeledLink label="Link to GitHub Gist:" link={gistLink} />
-        </>
-      )}
+      <GistState gistState={state.gist} />
     </>
   );
 };
