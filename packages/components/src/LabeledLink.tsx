e8e4f76cd82b0913bf8d50253b456e4cd6049132
nmauersberg <n.mauersberg@gmail.com>
2021-12-01 12:58:20 +0100

    Animate copy icon

--- packages/components/src/LabeledLink.tsx
+++ packages/components/src/LabeledLink.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 
 export type LabeledLinkProps = {
   label: string;
@@ -6,6 +6,15 @@ export type LabeledLinkProps = {
 };
 
 export const LabeledLink = ({ label, link }: LabeledLinkProps): ReactElement => {
+  const [animatePing, setAnimatePing] = useState('');
+
+  const showAnimation = (): void => {
+    setAnimatePing('animate-ping');
+    setTimeout(() => {
+      setAnimatePing('');
+    }, 750);
+  };
+
   return (
     <div className="dark:bg-elevation dark:border-dark border-light border-t last:rounded-b py-2 px-4 w-full text-lg">
       <p className="text-sm mt-1 mb-2 whitespace-nowrap">{label}</p>
@@ -15,11 +24,12 @@ export const LabeledLink = ({ label, link }: LabeledLinkProps): ReactElement => 
         </a>
         <button
           onClick={() => {
+            showAnimation();
             // Maybe we could use an extra lib for that
             navigator.clipboard.writeText(link);
           }}
         >
-          <i className="pi pi-copy ml-4" />
+          <i className={`pi pi-copy ml-4 ${animatePing}`} />
         </button>
       </div>
     </div>
