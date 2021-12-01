2afc0aa66107eb52b368c75f4e9749fc51e54844
nmauersberg <n.mauersberg@gmail.com>
2021-12-01 12:58:51 +0100

    Show env vars in dev mode

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -4,5 +4,10 @@ import './style/fonts.css';
 import './style/tailwind.css';
 import './style/global.css';
 import './style/scrollbar.css';
+import * as envs from '~/env';
+
+if (envs.NODE_ENV === 'development') {
+  console.log('Development env vars:', JSON.stringify(envs, null, 2));
+}
 
 ReactDOM.render(<App />, document.getElementById('container'));
