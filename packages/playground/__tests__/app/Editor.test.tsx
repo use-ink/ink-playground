7a562b9f78fcf738a1f03e7fcda6efd17cc3ac7d
Michael Bock <me@thought2.de>
2021-12-02 13:42:36 +0100

    Fix tests

--- packages/playground/__tests__/app/Editor.test.tsx
+++ packages/playground/__tests__/app/Editor.test.tsx
@@ -1,9 +1,9 @@
-import { Editor } from '~/app/Editor';
+import { InkEditor } from '@paritytech/ink-editor';
 import { render } from '@testing-library/react';
 
 jest.mock('monaco-editor/esm/vs/editor/editor.api.js');
 
 test('should render the monaco-editor', async () => {
-  const renderedEditor = render(<Editor />);
+  const renderedEditor = render(<InkEditor code={''} />);
   expect(renderedEditor).toMatchSnapshot();
 });
