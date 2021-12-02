46ecada4fc43dfbaefcd13595c53dbf6bbe76a1b
Michael Bock <me@thought2.de>
2021-12-02 13:39:01 +0100

    Use package's api

--- packages/playground/src/context/side-effects/compile.ts
+++ packages/playground/src/context/side-effects/compile.ts
@@ -1,4 +1,4 @@
-import { CompileApiResponse, compileRequest } from '~/api/compile';
+import { CompileApiResponse, compileRequest } from '@paritytech/ink-editor/api';
 import { State, Dispatch } from '~/context/app/reducer';
 import { MessageAction, MessageDispatch } from '~/context/messages/reducer';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
