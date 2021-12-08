cb75e1258bb608429a8002874a3a9be1101e59af
Michael Bock <me@thought2.de>
2021-12-08 19:56:04 +0100

    Finish config

--- packages/ink-editor/webpack.config.js
+++ packages/ink-editor/webpack.config.js
@@ -2,70 +2,67 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
-// const webpackConfig = {
-//   entry: {
-//     app: './src/index.tsx',
-//   },
-//   output: {
-//     filename: 'bundle.[fullhash].js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   resolve: {
-//     extensions: ['.ts', '.tsx', '.js', '.jsx', '.wasm', '.css'],
-//     alias: {
-//       '@paritytech/commontypes': '../commontypes/src',
-//     },
-//   },
-//   stats: 'errors-only',
-//   module: {
-//     rules: [
-//       {
-//         test: /.tsx?$/,
-//         use: 'ts-loader',
-//         exclude: ['/node_modules/'],
-//       },
-//       {
-//         test: /\.css$/,
-//         use: [MiniCssExtractPlugin.loader, 'css-loader'],
-//       },
-//       {
-//         test: /\.svg$/,
-//         use: [
-//           {
-//             loader: '@svgr/webpack',
-//             options: {
-//               typescript: true,
-//               ext: 'tsx',
-//             },
-//           },
-//         ],
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new MonacoWebpackPlugin({
-//       languages: ['rust'],
-//     }),
-//     // needed for monaco:
-//     new MiniCssExtractPlugin({
-//       filename: 'styles.css',
-//       chunkFilename: 'styles.css',
-//     }),
-//   ],
-//   devServer: {
-//     headers: {
-//       'Cross-Origin-Opener-Policy': 'same-origin',
-//       'Cross-Origin-Embedder-Policy': 'require-corp',
-//     },
-//     client: {
-//       overlay: {
-//         errors: true,
-//         warnings: false,
-//       },
-//     },
-//   },
-// };
+const webpackConfig = {
+  output: {
+    filename: 'bundle.[fullhash].js',
+    path: 'dist',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.wasm', '.css'],
+    alias: {
+      '@paritytech/commontypes': '../commontypes/src',
+    },
+  },
+  stats: 'errors-only',
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: 'ts-loader',
+        exclude: ['/node_modules/'],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              ext: 'tsx',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MonacoWebpackPlugin({
+      languages: ['rust'],
+    }),
+    // needed for monaco:
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+      chunkFilename: 'styles.css',
+    }),
+  ],
+  devServer: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
+};
 
-const webpackConfig = {};
+//const webpackConfig = {};
 
 module.exports = webpackConfig;
