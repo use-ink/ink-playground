2655422688b3f3b055de8d5b485d98147e62e65b
achimcc <achim@parity.io>
2021-12-13 18:05:18 +0100

    no pre-loaded image

--- on-start.sh
+++ on-start.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-dockerd > /var/log/dockerd.log 2>&1 & 
+# dockerd start
+dockerd > /var/log/dockerd.log 2>&1 &
+sleep 2
+
+# pull inner images
+docker pull achimcc/ink-backend:latest
+docker tag achimcc/ink-backend ink-backend
+
+# start backend server
 ./target/release/backend --port 4000 --host 0.0.0.0 --frontend_folder packages/playground/dist
