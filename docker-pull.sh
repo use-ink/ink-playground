9ed6e81a0d25768068b97d105efcf3101e350b57
achimcc <achim@parity.io>
2021-12-13 12:12:15 +0100

    configure dockerfille for sysbox

---
+++ docker-pull.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# dockerd start
+dockerd > /var/log/dockerd.log 2>&1 &
+sleep 2
+
+# pull inner images
+docker pull achimcc/ink-backend:latest
+
+# dockerd cleanup (remove the .pid file as otherwise it prevents
+# dockerd from launching correctly inside sys container)
+kill $(cat /var/run/docker.pid)
+kill $(cat /run/docker/containerd/containerd.pid)
+rm -f /var/run/docker.pid
+rm -f /run/docker/containerd/containerd.pid
