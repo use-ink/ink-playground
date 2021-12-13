5847c24876c420fc2020e4ec5c0033cefc9c6e3b
achimcc <achim@parity.io>
2021-12-13 13:11:06 +0100

    try other kernel version

--- install-shiftfs.sh
+++ install-shiftfs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~
-git clone --branch=k5.4 git://github.com/toby63/shiftfs-dkms.git shiftfs-dkms
+git clone --branch=k5.10 git://github.com/toby63/shiftfs-dkms.git shiftfs-dkms
 cd shiftfs-dkms
 sudo make -f Makefile.dkms
 sudo find /lib/modules/$(uname -r)/ -iname \"*shiftf*\"
