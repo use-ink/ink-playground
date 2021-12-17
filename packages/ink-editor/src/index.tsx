77ad98d8bc3d578405531241d8e39b3ca05e7750
nmauersberg <n.mauersberg@gmail.com>
2021-12-17 16:22:17 +0100

    Add custom token colors

--- packages/ink-editor/src/index.tsx
+++ packages/ink-editor/src/index.tsx
@@ -3,6 +3,15 @@ import MonacoEditor, { MonacoEditorProps, monaco } from 'react-monaco-editor';
 import exampleCode from './example-code';
 import { Uri } from 'monaco-editor/esm/vs/editor/editor.api';
 
+const teal = '67c6b0';
+const lightTeal = '67c6b0';
+const white = 'ffffff';
+const green = '709950';
+const orange = 'ce9178';
+const blue = '6298d4';
+const lightBlue = '95d9fc';
+const yellow = 'dcdd9b';
+
 monaco.editor.defineTheme('custom-dark', {
   base: 'vs-dark',
   inherit: true,
@@ -10,7 +19,33 @@ monaco.editor.defineTheme('custom-dark', {
     // { token: 'comment', foreground: 'ffa500', fontStyle: 'italic underline' },
     // { token: 'comment.js', foreground: '008800', fontStyle: 'bold' },
     // { token: 'comment.css', foreground: '0000ff' } // will inherit fontStyle from `comment` above
-    { token: 'builtin_type', foreground: '54b0b0' },
+    { token: 'comment', foreground: green },
+    { token: 'none', foreground: white },
+    { token: 'attribute', foreground: white },
+    { token: 'builtin_attr', foreground: teal },
+    { token: 'parenthesis', foreground: white },
+    { token: 'operator', foreground: white },
+    { token: 'string_literal', foreground: orange },
+    { token: 'comma', foreground: white },
+    { token: 'keyword', foreground: blue },
+    { token: 'module', foreground: teal },
+    { token: 'semicolon', foreground: white },
+    { token: 'brace', foreground: white },
+    { token: 'struct', foreground: lightTeal },
+    { token: 'field', foreground: lightBlue },
+    { token: 'colon', foreground: white },
+    { token: 'builtin_type', foreground: lightTeal },
+    { token: 'function', foreground: yellow },
+    { token: 'value_param', foreground: lightTeal },
+    { token: 'self_type', foreground: teal },
+    { token: 'trait', foreground: teal },
+    { token: 'self_keyword', foreground: blue },
+    { token: 'logical', foreground: white },
+    { token: 'punctuation', foreground: white },
+    { token: 'variable', foreground: lightBlue },
+    { token: 'unresolved_reference', foreground: blue },
+    { token: 'macro', foreground: blue },
+    { token: 'bool_literal', foreground: lightBlue },
   ],
   colors: {
     'editor.background': '#1A1D1F',
