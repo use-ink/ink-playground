603570b9984fa8bf713a062a295df758903d2531
nmauersberg <n.mauersberg@gmail.com>
2021-12-22 12:04:50 +0100

    Remove Editor test

--- packages/playground/__tests__/app/Editor.test.tsx
+++
@@ -1,9 +0,0 @@
-import { InkEditor } from '../../../ink-editor/src';
-import { render } from '@testing-library/react';
-
-jest.mock('monaco-editor/esm/vs/editor/editor.api.js');
-
-test('should render the monaco-editor', async () => {
-  const renderedEditor = render(<InkEditor code={''} />);
-  expect(renderedEditor).toMatchSnapshot();
-});
