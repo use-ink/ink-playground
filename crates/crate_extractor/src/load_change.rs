fb96ffe6175eff30bca0b2aeaf788c067b887fbd
achimcc <achim@parity.io>
2022-02-08 15:06:32 +0100

    rust-fmt

--- crates/crate_extractor/src/load_change.rs
+++ crates/crate_extractor/src/load_change.rs
@@ -78,13 +78,16 @@ pub fn load_change(
         },
     );
 
-    let crate_graph =
-        ws.to_crate_graph(&Default::default(), &mut |_, _| Vec::new(), &mut |path: &AbsPath| {
+    let crate_graph = ws.to_crate_graph(
+        &Default::default(),
+        &mut |_, _| Vec::new(),
+        &mut |path: &AbsPath| {
             let contents = loader.load_sync(path);
             let path = vfs::VfsPath::from(path.to_path_buf());
             vfs.set_file_contents(path.clone(), contents);
             vfs.file_id(&path)
-        });
+        },
+    );
 
     let project_folders = ProjectFolders::new(&[ws], &[]);
     loader.set_config(vfs::loader::Config {
