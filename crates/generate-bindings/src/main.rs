46208055f9925c70f4e8e3a5961bf4788b307a03
achimcc <achim@parity.io>
2022-03-17 10:09:45 +0100

    add new route/endpoint to backend

--- crates/generate-bindings/src/main.rs
+++ crates/generate-bindings/src/main.rs
@@ -24,8 +24,8 @@ fn main() -> std::io::Result<()> {
     let target = format!("{}/index.d.ts", &target);
 
     export_here! {
-        backend::services::compile::CompilationResult,
-        backend::services::compile::CompilationRequest,
+        backend::services::contract::CompilationResult,
+        backend::services::contract::CompilationRequest,
 
         backend::services::gist::common::Gist,
 
