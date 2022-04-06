19205dbfde9ecc4ba89a528fe6564f05c369abe0
achimcc <achim@parity.io>
2022-04-06 16:46:58 +0200

    first pull then start

--- sysbox/on-start.sh
+++ sysbox/on-start.sh
@@ -4,9 +4,10 @@
 dockerd > /var/log/dockerd.log 2>&1 &
 sleep 2
 
-# start backend server
-/app/target/release/backend --port 4000 --host 0.0.0.0 --frontend_folder /app/packages/playground/dist
-
 # pull inner images
 docker pull achimcc/ink-backend:latest
 docker tag achimcc/ink-backend ink-backend
+
+# start backend server
+/app/target/release/backend --port 4000 --host 0.0.0.0 --frontend_folder /app/packages/playground/dist
+
