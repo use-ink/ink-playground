1cf30f70b583226637a45fcd342982eaf6c3d3ac
achimcc <achim@parity.io>
2022-05-04 22:38:20 +0200

    remove obvious type annotation

--- packages/playground/src/constants.ts
+++ packages/playground/src/constants.ts
@@ -6,8 +6,8 @@ export const PROBLEMATIC_SIZE = 60;
 
 export const INCOMPATIBLE_SIZE = 80;
 
-export const CONTRACTS_UI_URL: string = 'https://paritytech.github.io/contracts-ui/';
+export const CONTRACTS_UI_URL = 'https://paritytech.github.io/contracts-ui/';
 
-export const INK_DOCS_URL: string = 'https://paritytech.github.io/ink-docs/';
+export const INK_DOCS_URL = 'https://paritytech.github.io/ink-docs/';
 
-export const REPO_URL: string = 'https://github.com/paritytech/ink-playground';
+export const REPO_URL = 'https://github.com/paritytech/ink-playground';
