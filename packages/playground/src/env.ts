937fa14d210bd55d49698aebcf29334816a6af2c
achimcc <achim@parity.io>
2022-05-04 16:17:16 +0200

    Move URLs to env definitions

--- packages/playground/src/env.ts
+++ packages/playground/src/env.ts
@@ -7,3 +7,10 @@ export const GIST_CREATE_URL: string | undefined = process.env.GIST_CREATE_URL;
 export const GIST_LOAD_URL: string | undefined = process.env.GIST_LOAD_URL;
 
 export const NODE_ENV = process.env.NODE_ENV as 'production' | 'development';
+
+export const CONTRACTS_UI_URL =
+  process.env.CONTRACTS_UI_URL || 'https://paritytech.github.io/contracts-ui/';
+
+export const INK_DOCS_URL = process.env.INK_DOCS_URL || 'https://paritytech.github.io/ink-docs/';
+
+export const REPO_URL = process.env.REPO_URL || 'https://github.com/paritytech/ink-playground';
