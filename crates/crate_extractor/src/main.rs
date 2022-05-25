e223de91de5c9111d5e51220070424089dfab0f9
achimcc <achim@parity.io>
2022-05-25 11:45:38 +0200

    remove format_args_capture flag from crate_extractor

--- crates/crate_extractor/src/main.rs
+++ crates/crate_extractor/src/main.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(format_args_capture)]
 use anyhow::Result;
 use change_json::ChangeJson;
 use std::{
