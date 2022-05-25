cf15bdfc9d1073ad70a42ff5914148f0b7cd54da
achimcc <achim@parity.io>
2022-05-25 11:26:45 +0200

    add enable_private_editable field to CompletionConfig

--- crates/rust_analyzer_wasm/src/lib.rs
+++ crates/rust_analyzer_wasm/src/lib.rs
@@ -158,6 +158,7 @@ impl WorldState {
             enable_postfix_completions: true,
             enable_imports_on_the_fly: true,
             enable_self_on_the_fly: true,
+            enable_private_editable: true,
             add_call_parenthesis: true,
             add_call_argument_snippets: true,
             snippet_cap: SnippetCap::new(true),
