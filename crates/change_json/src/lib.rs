23d799ee02e2bdeb7abce8d4922148fb935be40b
achimcc <achim@parity.io>
2022-06-06 16:05:27 +0200

    impl Default

--- crates/change_json/src/lib.rs
+++ crates/change_json/src/lib.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.#![feature(derive_default_enum)]
 
-#![feature(derive_default_enum)]
-
 mod change_json;
 mod crate_graph_json;
 
