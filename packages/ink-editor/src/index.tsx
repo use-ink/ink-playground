4feb9e0fada66eab6d62fc22226bcc2d8d64a2f2
achimcc <achim@parity.io>
2022-07-15 13:04:20 +0200

    fix imports

--- packages/ink-editor/src/index.tsx
+++ packages/ink-editor/src/index.tsx
@@ -3,6 +3,8 @@ import { InkEditor, InkEditorProps } from './ink-editor';
 import { compileRequest } from './api/compile';
 import { gistCreateRequest, gistLoadRequest } from './api/gists';
 import { testingRequest } from './api/testing';
+import monaco from 'monaco-editor';
+import { Uri } from 'monaco-editor/esm/vs/editor/editor.api';
 import type { GistCreateApiResponse } from './api/gists';
 import type { CompileApiResponse } from './api/compile';
 import type { TestingApiResponse } from './api/testing';
@@ -13,5 +15,7 @@ export {
   gistCreateRequest,
   gistLoadRequest,
   testingRequest,
+  monaco,
+  Uri,
 };
 export type { InkEditorProps, GistCreateApiResponse, CompileApiResponse, TestingApiResponse };
--- packages/playground/src/context/side-effects/testing.ts
+++ packages/playground/src/context/side-effects/testing.ts
@@ -1,7 +1,6 @@
-import { TestingApiResponse, testingRequest } from '@paritytech/ink-editor';
+import { TestingApiResponse, testingRequest, monaco } from '@paritytech/ink-editor';
 import { State, Dispatch } from '~/context/app/reducer';
 import { MessageAction, MessageDispatch } from '~/context/messages/reducer';
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import { TESTING_URL } from '~/env';
 
 const getMessageAction = (result: TestingApiResponse): MessageAction | undefined => {
