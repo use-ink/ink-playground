b246e00e4de3460a46df69c68e342af8cf7edac5
achimcc <achim@parity.io>
2022-07-19 11:35:57 +0200

    Revert "fix ink-editor imports"
    This reverts commit 5e798c3f8560a7240fec5035424c9fe925e889c9.

--- packages/ink-editor/src/index.tsx
+++ packages/ink-editor/src/index.tsx
@@ -1,17 +1,7 @@
 import exampleCode from './example-code';
 import { InkEditor, InkEditorProps } from './ink-editor';
-import { compileRequest } from './api/compile';
-import { gistCreateRequest, gistLoadRequest } from './api/gists';
-import { testingRequest } from './api/testing';
 import type { GistCreateApiResponse } from './api/gists';
 import type { CompileApiResponse } from './api/compile';
 import type { TestingApiResponse } from './api/testing';
-export {
-  exampleCode,
-  InkEditor,
-  compileRequest,
-  gistCreateRequest,
-  gistLoadRequest,
-  testingRequest,
-};
+export { exampleCode, InkEditor };
 export type { InkEditorProps, GistCreateApiResponse, CompileApiResponse, TestingApiResponse };
