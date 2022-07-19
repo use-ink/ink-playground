204e19aaa7ef89834e7712f186232b2bda337ac0
achimcc <achim@parity.io>
2022-07-19 11:35:33 +0200

    Revert "fix imports"
    This reverts commit e3445848905fa2d2fe79d2faca39afa1344b5f9f.

--- packages/ink-editor/src/index.tsx
+++ packages/ink-editor/src/index.tsx
@@ -3,8 +3,6 @@ import { InkEditor, InkEditorProps } from './ink-editor';
 import { compileRequest } from './api/compile';
 import { gistCreateRequest, gistLoadRequest } from './api/gists';
 import { testingRequest } from './api/testing';
-import monaco from 'monaco-editor';
-import { Uri } from 'monaco-editor/esm/vs/editor/editor.api';
 import type { GistCreateApiResponse } from './api/gists';
 import type { CompileApiResponse } from './api/compile';
 import type { TestingApiResponse } from './api/testing';
@@ -15,7 +13,5 @@ export {
   gistCreateRequest,
   gistLoadRequest,
   testingRequest,
-  monaco,
-  Uri,
 };
 export type { InkEditorProps, GistCreateApiResponse, CompileApiResponse, TestingApiResponse };
