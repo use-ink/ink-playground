087fda3865b4bc122ac0f831cb1844805e2925e0
achimcc <achim@parity.io>
2022-07-26 14:12:54 +0200

    fix wasm-pack tests

--- crates/rust_analyzer_wasm/tests/web.rs
+++ crates/rust_analyzer_wasm/tests/web.rs
@@ -61,7 +61,7 @@ pub fn create_crate(crate_graph: &mut CrateGraph, f: FileId) -> CrateId {
         cfg,
         CfgOptions::default(),
         Env::default(),
-        Default::default(),
+        Some(Default::default()),
         false,
         CrateOrigin::CratesIo { repo: None },
     )
