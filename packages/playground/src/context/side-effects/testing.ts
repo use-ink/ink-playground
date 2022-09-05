14c0fd28d2f87207c45bd6b79bb3c9276f728891
Achim Schneider <achim.schneider@posteo.de>
2022-09-05 02:18:31 +0200

    proper error checking for test response

--- packages/playground/src/context/side-effects/testing.ts
+++ packages/playground/src/context/side-effects/testing.ts
@@ -46,7 +46,9 @@ const getMessageAction = (result: TestingApiResponse): MessageAction | undefined
 
 function interpret_response(response: TestingApiResponse): TestingApiResponse {
   if (response.type === 'OK' && response.payload.type === 'SUCCESS') {
-    const has_test_error = /FAILED/.test(response.payload.payload.stdout);
+    const has_test_error =
+      /FAILED/.test(response.payload.payload.stdout) ||
+      /error: could not compile/.test(response.payload.payload.stdout);
     if (has_test_error) {
       const result = Object.assign(response, {
         ...response,
