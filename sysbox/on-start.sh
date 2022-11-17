40e5488825af8a951ef3ad25f88aaccafb81ea5d
Achim Schneider <achim.schneider@posteo.de>
2022-11-17 18:28:26 +0100

    pull paritytech ink-ci-linux

--- sysbox/on-start.sh
+++ sysbox/on-start.sh
@@ -5,8 +5,8 @@ dockerd > /var/log/dockerd.log 2>&1 &
 sleep 2
 
 # pull inner images
-docker pull achimcc/ink-compiler:latest
-docker tag achimcc/ink-compiler ink-compiler
+docker pull paritytech/ink-ci-linux:production
+docker tag paritytech/ink-ci-linux ink-compiler
 
 # start backend server
 /app/target/release/backend --port 4000 --host 0.0.0.0 --frontend_folder /app/packages/playground/dist
