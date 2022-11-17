ac855f8656fa918c48ac21d178879cf0fb516c3a
Achim Schneider <achim.schneider@posteo.de>
2022-11-17 18:34:35 +0100

    pr-build cached dependencies

--- sysbox/on-start.sh
+++ sysbox/on-start.sh
@@ -8,6 +8,8 @@ sleep 2
 docker pull paritytech/ink-ci-linux:production
 docker tag paritytech/ink-ci-linux ink-compiler
 
+docker run --volume cache:/usr/local/cargo/registry:ro --workdir /builds/contract/  ink-compiler cargo contract build
+
 # start backend server
 /app/target/release/backend --port 4000 --host 0.0.0.0 --frontend_folder /app/packages/playground/dist
 
