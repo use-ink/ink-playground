2139e81e0eff5118b8b27b063047ad9c0863da31
Achim Schneider <achim.schneider@posteo.de>
2022-11-22 20:26:36 +0100

    fix CI

--- sysbox/on-start.sh
+++ sysbox/on-start.sh
@@ -8,7 +8,7 @@ sleep 2
 docker pull paritytech/ink-ci-linux:production
 docker tag paritytech/ink-ci-linux:production ink-compiler
 
-docker run --volume cache:/usr/local/cargo/registry --volume /builds:/builds --workdir /builds/contract/  ink-compiler cargo contract build
+docker run --volume cache:/usr/local/cargo/registry --volume /builds:/builds --workdir /builds/contract/  ink-compiler cargo contract build --release
 docker run --volume cache:/usr/local/cargo/registry --volume /builds:/builds --workdir /builds/contract/  ink-compiler cargo contract test
 # start backend server
 /app/target/release/backend --port 4000 --host 0.0.0.0 --frontend_folder /app/packages/playground/dist
