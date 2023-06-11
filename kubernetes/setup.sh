c20f36f6c65292eaa722cd9f1b5482eaf825ca28
Achim Schneider <achim@parity.io>
2023-06-11 15:05:19 +0200

    CI: fix minikube invocation

---
+++ kubernetes/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+################################################################################
+# Prepare minikube
+################################################################################
+
+minikube stop
+minikube delete
+minikube start
+minikube image load ink-playground-kubernetes:latest
+
+################################################################################
+# Apply kubernetes rules
+################################################################################
+
+minikube kubectl -- apply -f serviceaccount.yaml
+minikube kubectl -- apply -f clusterrolebinding.yaml
+minikube kubectl -- apply -f pod.yaml
+minikube kubectl -- apply -f service.yaml
